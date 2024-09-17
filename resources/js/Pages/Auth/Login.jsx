6242bc16f9ea5744389b94fedbe819ed11afac26
kamba tukebele <kambatukebele@gmail.com>
2024-09-17 21:53:54 +0200

    updated UI Login.jsx

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -5,6 +5,8 @@ import InputLabel from '@/Components/InputLabel';
 import PrimaryButton from '@/Components/PrimaryButton';
 import TextInput from '@/Components/TextInput';
 import { Head, Link, useForm } from '@inertiajs/react';
+import ladder from '../../../../public/ladder.svg';
+import ladder_hat from '../../../../public/ladder-hat.svg';
 
 export default function Login({ status, canResetPassword }) {
     const { data, setData, post, processing, errors, reset } = useForm({
@@ -22,12 +24,87 @@ export default function Login({ status, canResetPassword }) {
     };
 
     return (
-        <GuestLayout>
+        <>
             <Head title="Log in" />
-
             {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
+            <section className='bg-gray-50 w-full h-screen relative'>
+                <form onSubmit={submit} className='container h-fit mx-auto absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 lg:flex lg:justify-center lg:items-center'>
+                    <div className='hidden lg:!block'>
+                        <div>
+                            <img src={ladder} alt="image-ladder" />
+                        </div>
+                        <div className='lg:absolute lg:top-[-30px] lg:right-0 xl:right-32'>
+                            <img className='w-20 rotate-90' src={ladder_hat} alt="image-ladder-hat" />
+                        </div>
+                    </div>
+                    <div className='p-4 bg-orange-400 min-w-[190px] rounded-3xl mx-4 sm:min-w-0 sm:w-[450px] sm:mx-auto lg:px-8'>
+                        <div className='flex flex-col gap-3 mb-10'>
+                            <h1 className='text-2xl font-extrabold text-white text-center lg:text-3xl'>Get more things done with Loggin platform.</h1>
+                            <p className='text-base font-normal text-white text-wrap text-center'>
+                                Access to the most powerfull tool in the entire design and web industry.
+                            </p>
+                        </div>
+                       
+                        <div className='flex flex-col justify-center gap-1 mb-2'>
+                            <label className='text-white text-sm' htmlFor="email">Email</label>
+                            <input 
+                                id="email"
+                                type="email"
+                                name="email"
+                                value={data.email}
+                                autoComplete="username"
+                                onChange={(e) => setData('email', e.target.value)}
+                                required 
+                                className='rounded-lg py-3 border-none text-sm font-thin'
+                                placeholder='Enter your email here'
+                            />
+                            <InputError message={errors.email} className="mt-2" />
+                        </div>
+                        <div className='flex flex-col justify-center gap-1 mb-2'>
+                            <label className='text-white text-sm' htmlFor="password">Password</label>
+                            <input 
+                                id="password"
+                                type="password"
+                                name="password"
+                                value={data.password}
+                                autoComplete="new-password"
+                                onChange={(e) => setData('password', e.target.value)}
+                                required 
+                                className='rounded-lg py-3 border-none text-sm font-thin' 
+                                placeholder='Enter your password here'
+                            />
+                            <InputError message={errors.password} className="mt-2" />
+                        </div>
+                        
+                        <div className="block mt-4">
+                            <label className="flex items-center">
+                                <Checkbox
+                                    name="remember"
+                                    checked={data.remember}
+                                    onChange={(e) => setData('remember', e.target.checked)}
+                                />
+                                <span className="ms-2 text-sm text-gray-600">Remember me</span>
+                            </label>
+                        </div>
 
-            <form onSubmit={submit}>
+                        <div className="flex items-center justify-end mt-4">
+                            {canResetPassword && (
+                            <Link
+                                href={route('password.request')}
+                                className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            >
+                                Forgot your password?
+                                </Link>
+                            )}
+
+                            <PrimaryButton className="ms-4" disabled={processing}>
+                                Log in
+                            </PrimaryButton>
+                        </div>
+                    </div>
+                </form>
+            </section>
+            {/* <form onSubmit={submit}>
                 <div>
                     <InputLabel htmlFor="email" value="Email" />
 
@@ -61,32 +138,9 @@ export default function Login({ status, canResetPassword }) {
                     <InputError message={errors.password} className="mt-2" />
                 </div>
 
-                <div className="block mt-4">
-                    <label className="flex items-center">
-                        <Checkbox
-                            name="remember"
-                            checked={data.remember}
-                            onChange={(e) => setData('remember', e.target.checked)}
-                        />
-                        <span className="ms-2 text-sm text-gray-600">Remember me</span>
-                    </label>
+                
                 </div>
-
-                <div className="flex items-center justify-end mt-4">
-                    {canResetPassword && (
-                        <Link
-                            href={route('password.request')}
-                            className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        >
-                            Forgot your password?
-                        </Link>
-                    )}
-
-                    <PrimaryButton className="ms-4" disabled={processing}>
-                        Log in
-                    </PrimaryButton>
-                </div>
-            </form>
-        </GuestLayout>
+            </form> */}
+        </>
     );
 }
