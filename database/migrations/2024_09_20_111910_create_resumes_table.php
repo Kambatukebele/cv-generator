6efa3223e97906dd5c1d56b2c052d9cd9d8fa9c2
kamba tukebele <kambatukebele@gmail.com>
2024-09-20 13:26:31 +0200

    added row name in the resumes tables

--- database/migrations/2024_09_20_111910_create_resumes_table.php
+++ database/migrations/2024_09_20_111910_create_resumes_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('resumes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title')->nullable();
+            $table->json('sections');
             $table->timestamps();
         });
     }
