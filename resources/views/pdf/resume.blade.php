7aa9eb999908508675a5468debfa226cd54ed264
kambatukebele <kambacharles11@gmail.com>
2024-10-28 20:08:23 +0100

    updated2 Resume UI

--- resources/views/pdf/resume.blade.php
+++ resources/views/pdf/resume.blade.php
@@ -1,15 +1,26 @@
-<div style="max-width: 210mm; position: relative; z-index: 20; margin: auto; padding: 6px; background-color: #f9fafb; box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 3px 0px;">
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+    body {
+        font-family: 'Roboto', sans-serif;
+    }
+</style>
 
-    <div style="width: 80mm; float: left; margin-right: 10px;">
-        <div style="margin-bottom: 24px;">
-            <h3 style="font-size: 20px; font-weight: 600; color: #111827; margin-bottom: 16px;">Personal Detail</h3>
-            <p style="font-size: 16px; color: #374151;">Email: <span>{{$resume->contact_info[0]->email}}</span></p>
+
+<div style="max-width: 210mm; margin: auto; background-color: #f9fafb; position: relative;">
+    <div style="width: 70mm; float: left; margin-right:30px">
+        <div style="margin-bottom: 10px; ">
+            <h3 style="font-size: 20px; font-weight: 600; color: #111827; margin-bottom: 16px; text-decoration:underline; text-transform:uppercase">Personal Detail</h3>
+            <p style="font-size: 16px; color: #374151;">
+                <span>
+                    Email:
+                </span>
+                <span>{{$resume->contact_info[0]->email}}</span></p>
             <p style="font-size: 16px; color: #374151;">Phone: <span>{{$resume->contact_info[0]->phone}}</span></p>
             <p style="font-size: 16px; color: #374151;">Linkedin: <span>{{$resume->contact_info[0]->linkedin}}</span></p>
             <p style="font-size: 16px; color: #374151;">X: <span>{{$resume->contact_info[0]->twitter}}</span></p>
         </div>
-        <div style="margin-bottom: 24px;">
-            <h3 style="font-size: 20px; margin-bottom: 4px; font-weight: 600; color: #1f2937;">Education</h3>
+        <div style="margin-bottom: 10px;">
+            <h3 style="font-size: 20px; font-weight: 600; color: #111827; margin-bottom: 16px; text-decoration:underline; text-transform:uppercase">Education</h3>
             @foreach ($resume->educations as $education)
                 <div style="margin-bottom: 8px;">
                     <small style="font-size: 12px; padding: 3px 8px; background-color: #5b21b6; color: white; border-radius: 4px;">
@@ -20,9 +31,9 @@
                 </div>
             @endforeach
         </div>
-        <div style="margin-bottom: 24px;">
-            <h3 style="font-size: 20px; font-weight: 600; color: #111827;">Language</h3>
-            <table style=" width: 100%;">
+        <div style="margin-bottom: 10px;">
+            <h3 style="font-size: 20px; font-weight: 600; color: #111827; margin-bottom: 16px; text-decoration:underline; text-transform:uppercase">Language</h3>
+            <table>
                 <thead>
                     <tr>
                         <td style="width: 7%;  text-align: center; padding: 4px; background-color: #5b21b6; color: white;">Language</td>
@@ -39,8 +50,8 @@
                 </tbody>
             </table>
         </div>
-        <div style="margin-bottom: 24px;">
-            <h3 style="font-size: 20px; margin-bottom: 4px; font-weight: 600; color: #1f2937;">Project</h3>
+        <div style="margin-bottom: 10px;">
+            <h3 style="font-size: 20px; font-weight: 600; color: #111827; margin-bottom: 16px; text-decoration:underline; text-transform:uppercase">Project</h3>
             @foreach ($resume->projects as $project)
                 <div style="margin-bottom: 8px;">
                     <h4 style="font-size: 16px; color: #1f2937; font-weight: 600;">{{$project->title}}</h4>
@@ -51,17 +62,17 @@
         </div>
     </div>
 
-    <div style="width: 110mm; float: left;">
-        <div style="margin-bottom: 24px;">
+    <div style="width: 109mm; float: left;">
+        <div style="margin-bottom: 10px;">
             <h1 style="font-size: 32px; font-weight: bold;">{{$resume->contact_info[0]->first_name}} <span style="color: #5b21b6;">{{$resume->contact_info[0]->last_name}}</span></h1>
             <h4 style="font-size: 18px; color: #1f2937; font-weight: 600;">{{$resume->title}}</h4>
         </div>
-        <div style="margin-bottom: 24px;">
-            <h3 style="font-size: 20px; margin-bottom: 4px; font-weight: 600; color: #1f2937;">Career Objective</h3>
+        <div style="margin-bottom: 10px;">
+            <h3 style="font-size: 20px; font-weight: 600; color: #111827; margin-bottom: 16px; text-decoration:underline; text-transform:uppercase">Career Objective</h3>
             <p style="font-size: 14px; color: #374151;">{{$resume->summary}}</p>
         </div>
-        <div style="margin-bottom: 24px;">
-            <h3 style="font-size: 20px; margin-bottom: 4px; font-weight: 600; color: #1f2937;">Work Experience</h3>
+        <div style="margin-bottom: 10px;">
+            <h3 style="font-size: 20px; font-weight: 600; color: #111827; margin-bottom: 16px; text-decoration:underline; text-transform:uppercase">Work Experience</h3>
             @foreach ($resume->experiences as $experience)
                 <div style="margin-bottom: 8px;">
                     <small style="font-size: 12px; padding: 3px 8px; background-color: #5b21b6; color: white; border-radius: 4px;">
@@ -72,9 +83,9 @@
                 </div>
             @endforeach
         </div>
-        <div style="margin-bottom: 24px;">
-            <h3 style="font-size: 20px; font-weight: 600; color: #111827;">Technical Skills</h3>
-            <table style=" width: 100%;">
+        <div>
+            <h3 style="font-size: 20px; font-weight: 600; color: #111827; margin-bottom: 16px; text-decoration:underline; text-transform:uppercase">Technical Skills</h3>
+            <table>
                 <thead>
                     <tr>
                         <td style="width: 7%;  text-align: center; padding: 4px; background-color: #5b21b6; color: white;">Development</td>
@@ -92,5 +103,7 @@
             </table>
         </div>
     </div>
-    <div style="position: absolute; width: 176px; height: 176px; background-color: #f3e8ff; border-radius: 9999px; z-index: -10; top: 0; left: -112px;"></div>
+    <div style="display:block; width: 5mm; height:5mm; border-radius:50%; position: absolute; background-color:#5b21b6; top:10px; left:0px">
+
+    </div>
 </div>
